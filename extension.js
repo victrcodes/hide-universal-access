e4b3eb978149c0d9f068f206220ceaf4dcb99ef6
victrcodes <darodymas@gmail.com>
2026-03-13 21:28:12 +0200

    Hide accessibility indicator on all monitors instead of just the primary one.

--- extension.js
+++ extension.js
@@ -19,21 +19,95 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import GLib from "gi://GLib";
+
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
-import { panel } from "resource:///org/gnome/shell/ui/main.js";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 export default class HideUniversalAccess extends Extension {
+
   enable() {
-    let a11y = panel.statusArea["a11y"];
-    if (a11y != null) {
-      a11y.container.hide();
-    }
+    this._syncSourceId = 0;
+    this._monitorsChangedId = Main.layoutManager.connect(
+      "monitors-changed",
+      this._queueSync.bind(this),
+    );
+    this._extensionChangedId = Main.extensionManager.connect(
+      "extension-state-changed",
+      this._queueSync.bind(this),
+    );
+
+    this._setHidden(true);
   }
 
   disable() {
-    let a11y = panel.statusArea["a11y"];
-    if (a11y != null) {
-      a11y.container.show();
+    this._clearQueuedSync();
+
+    if (this._monitorsChangedId) {
+      Main.layoutManager.disconnect(this._monitorsChangedId);
+      this._monitorsChangedId = 0;
+    }
+
+    if (this._extensionChangedId) {
+      Main.extensionManager.disconnect(this._extensionChangedId);
+      this._extensionChangedId = 0;
+    }
+
+    this._setHidden(false);
+  }
+
+  _queueSync() {
+    if (this._syncSourceId) {
+      return;
+    }
+
+    this._syncSourceId = GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+      this._syncSourceId = 0;
+      this._setHidden(true);
+      return GLib.SOURCE_REMOVE;
+    });
+  }
+
+  _clearQueuedSync() {
+    if (this._syncSourceId) {
+      GLib.source_remove(this._syncSourceId);
+      this._syncSourceId = 0;
     }
   }
+
+  _setHidden(hidden) {
+    for (let indicator of this._getA11yIndicators()) {
+      if (hidden) {
+        indicator.container.hide();
+      } else {
+        indicator.container.show();
+      }
+    }
+  }
+
+  _getA11yIndicators() {
+    let indicators = [];
+    let seen = new Set();
+    let actors = [Main.panel, Main.uiGroup];
+
+    while (actors.length > 0) {
+      let actor = actors.pop();
+      let indicator = actor?.statusArea?.["a11y"];
+      let container = indicator?.container;
+
+      if (container != null && !seen.has(container)) {
+        seen.add(container);
+        indicators.push(indicator);
+      }
+
+      let children = actor?.get_children?.() ?? [];
+      for (let child of children) {
+        actors.push(child);
+      }
+    }
+
+    return indicators;
+
+  }
+
 }
